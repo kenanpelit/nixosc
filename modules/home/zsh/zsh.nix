620a455c79b33f14e73d5a16109eef9d03912c8d
Kenan Pelit <kenanpelit@gmail.com>
2025-01-03 23:27:18 +0300

    update

--- modules/home/zsh/zsh.nix
+++ modules/home/zsh/zsh.nix
@@ -100,6 +100,9 @@
      _fzf_compgen_path() { fd --hidden --exclude .git . "$1" }
      _fzf_compgen_dir() { fd --type=d --hidden --exclude .git . "$1" }
      
+     # Zoxide integration
+     eval "$(zoxide init zsh)"
+
      # Vi mode bindings
      bindkey -M vicmd 'k' up-line-or-beginning-search
      bindkey -M vicmd 'j' down-line-or-beginning-search
