9c7cb136d90330ceefc08d517dc872b40a4062fe
kenanpelit <kenanpelit@gmail.com>
2025-01-15 17:50:25 +0300

    fix(transmission): Set force=true for settings.json
    Add force=true attribute to transmission settings.json file
    configuration to ensure home-manager can overwrite existing files
    during activation, preventing file conflict errors.

--- modules/home/transmission/default.nix
+++ modules/home/transmission/default.nix
@@ -1,54 +1,56 @@
 # modules/home/transmission/default.nix
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
- # Transmission daemon ayarları
- home.file.".config/transmission-daemon/settings.json" = {
-   text = builtins.toJSON {
-     download-dir = "${config.home.homeDirectory}/Downloads/transmission";
-     incomplete-dir = "${config.home.homeDirectory}/Downloads/transmission/.incomplete";
-     incomplete-dir-enabled = true;
-     rpc-authentication-required = false;
-     rpc-bind-address = "127.0.0.1";
-     rpc-enabled = true;
-     rpc-port = 9091;
-     rpc-url = "/transmission/";
-     rpc-username = "";
-     rpc-password = "";
-     rpc-whitelist = "127.0.0.1";
-     rpc-whitelist-enabled = true;
-     speed-limit-down = 1000;
-     speed-limit-down-enabled = false;
-     speed-limit-up = 100;
-     speed-limit-up-enabled = false;
-     start-added-torrents = true;
-     trash-original-torrent-files = false;
-     umask = 18;
-     watch-dir = "${config.home.homeDirectory}/Downloads/transmission/watch";
-     watch-dir-enabled = true;
-   };
-   immutable = false;
- };
+  config = {
+    systemd.user.services.transmission = {
+      Unit = {
+        Description = "Transmission BitTorrent Daemon";
+        After = [ "network.target" ];
+      };
+      Service = {
+        Type = "notify";
+        ExecStartPre = pkgs.writeShellScript "transmission-setup" ''
+          mkdir -p ${config.home.homeDirectory}/Downloads/transmission/{complete,incomplete,watch}
+          mkdir -p ${config.home.homeDirectory}/.config/transmission-daemon
+        '';
+        ExecStart = "${pkgs.transmission_4}/bin/transmission-daemon -f --log-level=error";
+        ExecReload = "${pkgs.coreutils}/bin/kill -s HUP $MAINPID";
+        Restart = "on-failure";
+        RestartSec = "5s";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
 
- # User servisi olarak transmission
- systemd.user.services.transmission = {
-   Unit = {
-     Description = "Transmission BitTorrent Daemon";
-     After = [ "network.target" ];
-   };
-   Service = {
-     Type = "notify";
-     ExecStart = "${pkgs.transmission_4}/bin/transmission-daemon -f --log-level=error";
-     ExecReload = "${pkgs.coreutils}/bin/kill -s HUP $MAINPID";
-   };
-   Install = {
-     WantedBy = [ "default.target" ];
-   };
- };
+    home.file.".config/transmission-daemon/settings.json" = {
+      text = builtins.toJSON {
+        download-dir = "${config.home.homeDirectory}/Downloads/transmission/complete";
+        incomplete-dir = "${config.home.homeDirectory}/Downloads/transmission/incomplete";
+        incomplete-dir-enabled = true;
+        rpc-enabled = true;
+        rpc-port = 9091;
+        rpc-whitelist-enabled = true;
+        rpc-whitelist = "127.0.0.1";
+        watch-dir = "${config.home.homeDirectory}/Downloads/transmission/watch";
+        watch-dir-enabled = true;
+        speed-limit-down = 1000;
+        speed-limit-down-enabled = false;
+        speed-limit-up = 100;
+        speed-limit-up-enabled = false;
+        start-added-torrents = true;
+        trash-original-torrent-files = false;
+        umask = 18;
+        rpc-authentication-required = false;
+        rpc-username = "";
+        rpc-password = "";
+      };
+      force = true;
+    };
 
- # Gerekli dosya ve dizinleri oluştur
- home.file = {
-   "Downloads/transmission/.keep".text = "";
-   "Downloads/transmission/.incomplete/.keep".text = "";
-   "Downloads/transmission/watch/.keep".text = "";
- };
+    # Transmission paketi ekle
+    home.packages = with pkgs; [
+      transmission_4
+    ];
+  };
 }
