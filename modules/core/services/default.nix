c619b361872840bc2cb7d0e49b741eff58918021
kenanpelit <kenanpelit@gmail.com>
2025-01-15 16:51:32 +0300

    refactor(transmission): Move from system to user service
    Major changes:
    - Move transmission from system level to home-manager user service
    - Remove transmission configuration from core/services
    - Add systemd user service configuration in home module
    - Configure proper file permissions and user access
    - Set up download directories in user space
    
    This change improves security and user experience by running
    transmission as a user service instead of system service,
    eliminating the need for sudo permissions.

--- modules/core/services/default.nix
+++ modules/core/services/default.nix
@@ -1,35 +1,35 @@
-# modules/core/services/default.nix - Sistem seviyesi servisler
+# modules/core/services/default.nix
 { pkgs, ... }:
 {
+  # Temel sistem servisleri
   services = {
-    # Temel sistem servisleri
     gvfs.enable = true;                # Sanal dosya sistemi desteği
     fstrim.enable = true;              # SSD optimizasyonu
     dbus = {
       enable = true;
       packages = [ pkgs.gcr ];         # GPG ve şifreleme altyapısı için
     };
-    
-    # Touchégg hizmeti - dokunmatik hareketler için
-    touchegg.enable = false;
+    touchegg.enable = false;           # Touchégg hizmeti
     
     # systemd-logind yapılandırması
     logind.extraConfig = ''
-      # Güç düğmesine kısa basıldığında sistemi kapatma
       HandlePowerKey=ignore
     '';
   };
 
-  # Sistem genelinde gerekli programlar
-  programs.dconf.enable = true;        # Sistem ayarları veritabanı
-  
   # Sistem güvenliği
-  security = {
-    polkit.enable = true;              # Yetkilendirme sistemi
+  security.polkit.enable = true;
+
+  # Transmission için güvenlik duvarı kuralları
+  networking.firewall = {
+    allowedTCPPorts = [ 9091 ];       # Transmission web arayüzü
+    allowedTCPPortRanges = [{ 
+      from = 51413; 
+      to = 51413; 
+    }];
+    allowedUDPPortRanges = [{ 
+      from = 51413; 
+      to = 51413; 
+    }];
   };
-  
-  # Temel sistem paketleri
-  environment.systemPackages = with pkgs; [
-    gcr                                # GPG ve şifreleme altyapısı
-  ];
 }
