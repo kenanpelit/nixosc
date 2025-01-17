c417f75c802006ba43b3341f7e11342cff6a0469
kenanpelit <kenanpelit@gmail.com>
2025-01-17 20:55:55 +0300

    feat: move xsession error logs to ~/.cache/X11

--- modules/home/xserver/default.nix
+++ modules/home/xserver/default.nix
@@ -1,14 +1,21 @@
 # modules/home/xserver/default.nix
 { config, lib, pkgs, ... }:
 {
-  home.sessionVariables = {
+  systemd.user.sessionVariables = {
     USERXSESSION = "$HOME/.cache/X11/xsession";
     USERXSESSIONRC = "$HOME/.cache/X11/xsessionrc";
     ALTUSERXSESSION = "$HOME/.cache/X11/Xsession";
     ERRFILE = "$HOME/.cache/X11/xsession-errors";
   };
 
-  # Dizini otomatik oluşturmak için
+  # X11 dizinini oluştur
   home.file.".cache/X11/.keep".text = "";
-}
 
+  # Display manager'ın kullandığı scriptleri override et
+  home.file.".xprofile".text = ''
+    export USERXSESSION="$HOME/.cache/X11/xsession"
+    export USERXSESSIONRC="$HOME/.cache/X11/xsessionrc"
+    export ALTUSERXSESSION="$HOME/.cache/X11/Xsession"
+    export ERRFILE="$HOME/.cache/X11/xsession-errors"
+  '';
+}
