dc1c6f71a1f4cff479fb8dd1df1cf244ccfe8928
kenanpelit <kenanpelit@gmail.com>
2025-01-18 02:11:36 +0300

    add fzf preview

--- modules/home/fzf/default.nix
+++ modules/home/fzf/default.nix
@@ -2,20 +2,72 @@
 # ==============================================================================
 # FZF Configuration
 # ==============================================================================
-{ pkgs, ... }:
+# modules/home/fzf/default.nix
+{ pkgs, inputs, lib, ... }:
+
+let
+  previewDeps = with pkgs; [
+    # Temel araçlar
+    file
+    jq
+    bat
+    glow
+    w3m
+    eza
+    openssl
+    
+    # Arşiv araçları
+    atool
+    p7zip
+    libcdio
+    
+    # Doküman araçları
+    odt2txt
+    catdoc
+    gnumeric
+    
+    # Medya araçları
+    exiftool
+    chafa
+    mediainfo
+    ffmpegthumbnailer
+    poppler_utils
+  ];
+in
 {
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    
+
     # Temel arama komutları
     defaultCommand = "fd --hidden --strip-cwd-prefix --exclude .git";
-    
-    # Dosya önizleme ayarları
+
+    # Gelişmiş önizleme ayarları
     fileWidgetOptions = [
-      "--preview 'if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi'"
+      "--preview '
+        FILE=\"{}\"; 
+        if [ -L \"$FILE\" ]; then 
+          FILE=$(readlink -f \"$FILE\"); 
+        fi;
+        if [ -d \"$FILE\" ]; then 
+          eza --tree --color=always \"$FILE\" | head -200;
+        elif [[ \"$FILE\" =~ \\.(jpg|jpeg|png|gif|bmp)$ ]]; then
+          chafa --format symbols \"$FILE\";
+        elif [[ \"$FILE\" =~ \\.(pdf)$ ]]; then
+          pdftoppm -png -f 1 -l 1 \"$FILE\" - | chafa -;
+        elif [[ \"$FILE\" =~ \\.(mp4|webm|mkv)$ ]]; then
+          ffmpegthumbnailer -i \"$FILE\" -o - -s 0 | chafa -;
+        elif [[ \"$FILE\" =~ \\.(zip|tar|gz|bz2|rar|7z)$ ]]; then
+          atool --list \"$FILE\";
+        elif [[ \"$FILE\" =~ \\.(docx?|odt|xlsx?|ods)$ ]]; then
+          odt2txt \"$FILE\";
+        elif [[ \"$FILE\" =~ \\.(md|markdown)$ ]]; then
+          glow -s dark \"$FILE\";
+        else 
+          bat -n --color=always --line-range :500 \"$FILE\"; 
+        fi'"
     ];
-    
+
     # Dizin değiştirme ayarları
     changeDirWidgetCommand = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
     changeDirWidgetOptions = [
@@ -39,6 +91,14 @@
       # Bilgi konumu
       "--info='right'"
     ];
+  };
 
+  # Önizleme için gerekli bağımlılıkları yükle
+  home.packages = previewDeps;
+
+  # Önizleme için ortam değişkenleri
+  home.sessionVariables = {
+    FZF_PREVIEW_IMAGE_HANDLER = "chafa";  # veya "sixel" için terminalde sixel desteği varsa
   };
 }
+
