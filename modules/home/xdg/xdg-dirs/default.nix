9e9da989394bcb61b33c59e5f6f75029824a81db
kenanpelit <kenanpelit@gmail.com>
2025-01-19 23:24:46 +0300

    fix(xdg): Remove conflicting XDG environment variable definitions

--- modules/home/xdg/xdg-dirs/default.nix
+++ modules/home/xdg/xdg-dirs/default.nix
@@ -10,20 +10,20 @@
     createDirectories = true;  # Olmayan dizinleri oluştur
     
     # Temel dizinler
-    desktop = "$HOME/desktop";
-    documents = "$HOME/documents";
-    download = "$HOME/downloads";
-    music = "$HOME/music";
-    pictures = "$HOME/pictures";
-    videos = "$HOME/videos";
-    templates = "$HOME/templates";
-    publicShare = "$HOME/public";
+    desktop = "$HOME/Desktop";
+    documents = "$HOME/Documents";
+    download = "$HOME/Downloads";
+    music = "$HOME/Music";
+    pictures = "$HOME/Pictures";
+    videos = "$HOME/Videos";
+    templates = "$HOME/Templates";
+    publicShare = "$HOME/Public";
     
     # Özel dizinler
     extraConfig = {
       XDG_BACKUP_DIR = "$HOME/.backup";
       XDG_PROJECTS_DIR = "$HOME/.projects";
-      XDG_TMP_DIR = "$HOME/tmp";
+      XDG_TMP_DIR = "$HOME/.tmp";
       XDG_CONFIG_DIR = "$HOME/.config";
       XDG_DATA_DIR = "$HOME/.local/share";
       XDG_STATE_DIR = "$HOME/.local/state";
