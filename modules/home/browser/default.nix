0a17108c02b79af2890dfcc0a2af6b41c62f57b5
kenanpelit <kenanpelit@gmail.com>
2025-01-20 01:13:24 +0300

    browser: add default

---
+++ modules/home/browser/default.nix
@@ -0,0 +1,20 @@
+# modules/home/browser/default.nix
+# ==============================================================================
+# Browser Configuration
+# ==============================================================================
+# This module manages browser configurations including:
+#
+# Components:
+# - Firefox:
+# - Zen:
+#
+# Author: Kenan Pelit
+# ==============================================================================
+{ config, lib, pkgs, ... }:
+
+{
+ imports = [
+   ./firefox
+   ./zen
+ ];
+}
