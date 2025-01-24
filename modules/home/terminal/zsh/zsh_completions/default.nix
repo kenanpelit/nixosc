bdb20b329c5fc230ec3d762fb4b09896f695b6a9
kenanpelit <kenanpelit@gmail.com>
2025-01-24 20:05:20 +0300

    fix(zsh): update _assh completion filename and function names

--- modules/home/terminal/zsh/zsh_completions/default.nix
+++ modules/home/terminal/zsh/zsh_completions/default.nix
@@ -2,7 +2,7 @@
 { config, ... }:
 {
   home.file = {
-    ".config/zsh/completions/_iwctl".source = ./_iwctl;
+    #".config/zsh/completions/_iwctl".source = ./_iwctl;
     ".config/zsh/completions/_assh".source = ./_assh;
     # Diğer completion dosyaları için:
     # ".config/zsh/completions/other-completion.zsh".source = ./other-completion.zsh;
