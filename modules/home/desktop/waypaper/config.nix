ed73f8a3cb9f8f0a5f67f9a96d068e398b6939bb
kenanpelit <kenanpelit@gmail.com>
2025-01-25 16:34:45 +0300

    feat(waypaper): add dynamic wallpaper config for multi-monitor setup with periodic changes

--- modules/home/desktop/waypaper/config.nix
+++ modules/home/desktop/waypaper/config.nix
@@ -4,45 +4,49 @@
 # ==============================================================================
 { pkgs, ... }:
 {
-  # =============================================================================
-  # Package Installation
-  # =============================================================================
-  home.packages = (with pkgs; [ waypaper ]);
-
-  # =============================================================================
-  # Configuration
-  # =============================================================================
-  xdg.configFile."waypaper/config.ini".text = ''
-    [Settings]
-    # Basic Settings
-    language = en
-    folder = ~/Pictures/wallpapers/others
-    monitors = All
-    wallpaper = ~/Pictures/wallpapers/nixos/nixos_blue.png
-    
-    # Display Settings
-    backend = swww
-    fill = fill
-    sort = name
-    color = #ffffff
-    
-    # Folder Settings
-    subfolders = False
-    show_hidden = False
-    show_gifs_only = False
-    
-    # UI Settings
-    number_of_columns = 3
-    
-    # SWWW Settings
-    swww_transition_type = any
-    swww_transition_step = 90
-    swww_transition_angle = 0
-    swww_transition_duration = 2
-    swww_transition_fps = 60
-    
-    # Additional Settings
-    post_command = pkill .waypaper-wrapp
-    use_xdg_state = False
-  '';
+ # =============================================================================
+ # Package Installation
+ # =============================================================================
+ home.packages = (with pkgs; [ waypaper ]);
+ 
+ # =============================================================================
+ # Configuration
+ # =============================================================================
+ xdg.configFile."waypaper/config.ini".text = ''
+   [Settings]
+   # Monitor Settings
+   language = en
+   folder_eDP-1 = ~/Pictures/wallpapers/others
+   folder_DP-5 = ~/Pictures/wallpapers/others
+   monitors = Specific
+   
+   # Display Settings
+   backend = swww
+   fill = fill
+   sort = random
+   color = #ffffff
+   
+   # Dynamic Wallpaper Settings
+   change_mode = time
+   change_time = 5
+   
+   # Folder Settings
+   subfolders = False
+   show_hidden = False
+   show_gifs_only = False
+   
+   # UI Settings
+   number_of_columns = 3
+   
+   # SWWW Settings
+   swww_transition_type = any
+   swww_transition_step = 90
+   swww_transition_angle = 0
+   swww_transition_duration = 2
+   swww_transition_fps = 60
+   
+   # Additional Settings
+   post_command = pkill .waypaper-wrapp
+   use_xdg_state = False
+ '';
 }
