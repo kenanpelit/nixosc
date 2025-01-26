30e59c973a5bbe2e9a95d9a56791f7101628d523
kenanpelit <kenanpelit@gmail.com>
2025-01-26 14:13:29 +0300

    Spotify: set tokyonight

--- modules/home/media/spicetify/default.nix
+++ modules/home/media/spicetify/default.nix
@@ -1,34 +1,44 @@
-# modules/home/spicetify/default.nix
+# modules/home/media/spicetify/default.nix
 # ==============================================================================
 # Spicetify Spotify Client Configuration
 # ==============================================================================
 { pkgs, lib, inputs, ... }:
 let
-  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+ spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in
 {
-  # =============================================================================
-  # Package Configuration
-  # =============================================================================
-  nixpkgs.config.allowUnfreePredicate =
-    pkg: builtins.elem (lib.getName pkg) [ "spotify" ];
-  
-  # =============================================================================
-  # Module Imports
-  # =============================================================================
-  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
-
-  # =============================================================================
-  # Spicetify Settings
-  # =============================================================================
-  programs.spicetify = {
-    enable = true;
-    enabledExtensions = with spicePkgs.extensions; [
-      adblock
-      hidePodcasts
-      shuffle
-    ];
-    theme = spicePkgs.themes.dribbblish;
-    colorScheme = "catppuccin-mocha";
-  };
+ # =============================================================================
+ # Package Configuration
+ # =============================================================================
+ nixpkgs.config.allowUnfreePredicate =
+   pkg: builtins.elem (lib.getName pkg) [ "spotify" ];
+ 
+ # =============================================================================
+ # Module Imports
+ # =============================================================================
+ imports = [ inputs.spicetify-nix.homeManagerModules.default ];
+ # =============================================================================
+ # Spicetify Settings
+ # =============================================================================
+ programs.spicetify = {
+   enable = true;
+   theme = {
+     name = "TokyoNight";
+     src = pkgs.fetchFromGitHub {
+       owner = "kenanpelit";
+       repo = "Spotify-TokyoNight";
+       rev = "d88ca06eaeeb424d19e0d6f7f8e614e4bce962be";
+       sha256 = "02aw8kvk4m7radsywpl10gq8x5g23xj5gwspyiawf7mdrazzvf3h";
+     };
+     injectCss = true;
+     replaceColors = true;
+     overwriteAssets = true;
+     colorScheme = "night";  # night, storm veya day
+   };
+   enabledExtensions = with spicePkgs.extensions; [
+     adblock
+     hidePodcasts
+     shuffle
+   ];
+ };
 }
