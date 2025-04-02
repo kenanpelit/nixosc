d517679a4253705eb8680a372f9937b1b0515bcb
kenanpelit <kenanpelit@gmail.com>
2025-04-02 09:05:01 +0300

    hyprland: 0402 - 5958 Commits

--- flake.nix
+++ flake.nix
@@ -70,7 +70,9 @@
    # Core Hyprland compositor
    hyprland = {
      #url = "github:hyprwm/hyprland/v0.48.1";  # Pin to specific v0.48.1 release
-     url = "github:hyprwm/hyprland/d1a59ec39eb4c0d6a7d3d38a26f8924e6bca5cef"; # 0401 - 5955 Commits
+     #url = "github:hyprwm/hyprland/d1a59ec39eb4c0d6a7d3d38a26f8924e6bca5cef"; # 0401 - 5955 Commits
+     url = "github:hyprwm/hyprland/a4e6c5d678e8dd27ab07a6d6eb4ba2834fab81d1"; # 0402 - 5958 Commits
+
      inputs.nixpkgs.follows = "nixpkgs";
    };
 
