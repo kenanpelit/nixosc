285c40a3efca45a688aa36ff74d18bf3ee5e8d3c
kenanpelit <kenanpelit@gmail.com>
2025-04-08 01:11:03 +0300

    gaps: 0

--- modules/home/desktop/hyprland/config.nix
+++ modules/home/desktop/hyprland/config.nix
@@ -146,8 +146,8 @@
       # =====================================================
       general = {
         "$mainMod" = "SUPER";                # Ana modifikatör tuşu
-        gaps_in = 5;
-        gaps_out = 10;
+        gaps_in = 0;
+        gaps_out = 0;
         border_size = 2;
         "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
         "col.inactive_border" = "rgba(595959aa)";
