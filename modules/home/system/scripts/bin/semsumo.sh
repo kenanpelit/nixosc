54ebcc40afc7edf6767923361a045d2c65548a36
kenanpelit <kenanpelit@gmail.com>
2025-05-03 21:25:49 +0300

    reorganization

--- modules/home/system/scripts/bin/semsumo.sh
+++ modules/home/system/scripts/bin/semsumo.sh
@@ -373,7 +373,7 @@ create_default_config() {
       "command": "ferdium",
       "args": [],
       "vpn": "secure",
-      "workspace": "5",
+      "workspace": "9",
       "fullscreen": true,
       "wait_time": 1,
       "enabled": true,
