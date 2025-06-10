9698be4b23d3bc55e24bbe4cef76e7bd07ccdddb
kenanpelit <kenanpelit@gmail.com>
2025-06-10 16:02:36 +0300

    fix: remove strict mode to resolve file I/O issues during script generation

--- modules/home/system/scripts/start.nix
+++ modules/home/system/scripts/start.nix
@@ -9,17 +9,11 @@ let
   start-brave-discord = pkgs.writeShellScriptBin "start-brave-discord" (
     builtins.readFile ./start/start-brave-discord.sh
   );
-  start-brave-exclude = pkgs.writeShellScriptBin "start-brave-exclude" (
-    builtins.readFile ./start/start-brave-exclude.sh
-  );
   start-brave-kenp = pkgs.writeShellScriptBin "start-brave-kenp" (
     builtins.readFile ./start/start-brave-kenp.sh
   );
   start-brave-spotify = pkgs.writeShellScriptBin "start-brave-spotify" (
     builtins.readFile ./start/start-brave-spotify.sh
-  );
-  start-brave-tiktok = pkgs.writeShellScriptBin "start-brave-tiktok" (
-    builtins.readFile ./start/start-brave-tiktok.sh
   );
   start-brave-whatsapp = pkgs.writeShellScriptBin "start-brave-whatsapp" (
     builtins.readFile ./start/start-brave-whatsapp.sh
@@ -29,18 +23,6 @@ let
   );
   start-brave-youtube = pkgs.writeShellScriptBin "start-brave-youtube" (
     builtins.readFile ./start/start-brave-youtube.sh
-  );
-  start-chrome-ai = pkgs.writeShellScriptBin "start-chrome-ai" (
-    builtins.readFile ./start/start-chrome-ai.sh
-  );
-  start-chrome-compecta = pkgs.writeShellScriptBin "start-chrome-compecta" (
-    builtins.readFile ./start/start-chrome-compecta.sh
-  );
-  start-chrome-kenp = pkgs.writeShellScriptBin "start-chrome-kenp" (
-    builtins.readFile ./start/start-chrome-kenp.sh
-  );
-  start-chrome-whats = pkgs.writeShellScriptBin "start-chrome-whats" (
-    builtins.readFile ./start/start-chrome-whats.sh
   );
   start-discord = pkgs.writeShellScriptBin "start-discord" (
     builtins.readFile ./start/start-discord.sh
@@ -56,9 +38,6 @@ let
   );
   start-mkenp = pkgs.writeShellScriptBin "start-mkenp" (
     builtins.readFile ./start/start-mkenp.sh
-  );
-  start-mpv = pkgs.writeShellScriptBin "start-mpv" (
-    builtins.readFile ./start/start-mpv.sh
   );
   start-spotify = pkgs.writeShellScriptBin "start-spotify" (
     builtins.readFile ./start/start-spotify.sh
@@ -87,12 +66,6 @@ let
   start-zen-novpn = pkgs.writeShellScriptBin "start-zen-novpn" (
     builtins.readFile ./start/start-zen-novpn.sh
   );
-  start-zen-proxy = pkgs.writeShellScriptBin "start-zen-proxy" (
-    builtins.readFile ./start/start-zen-proxy.sh
-  );
-  start-zen-spotify = pkgs.writeShellScriptBin "start-zen-spotify" (
-    builtins.readFile ./start/start-zen-spotify.sh
-  );
   start-zen-whats = pkgs.writeShellScriptBin "start-zen-whats" (
     builtins.readFile ./start/start-zen-whats.sh
   );
@@ -101,23 +74,16 @@ in {
     start-brave-ai
     start-brave-compecta
     start-brave-discord
-    start-brave-exclude
     start-brave-kenp
     start-brave-spotify
-    start-brave-tiktok
     start-brave-whatsapp
     start-brave-whats
     start-brave-youtube
-    start-chrome-ai
-    start-chrome-compecta
-    start-chrome-kenp
-    start-chrome-whats
     start-discord
     start-ferdium
     start-kitty-single
     start-kkenp
     start-mkenp
-    start-mpv
     start-spotify
     start-webcord
     start-wezterm-rmpc
@@ -127,8 +93,6 @@ in {
     start-zen-discord
     start-zen-kenp
     start-zen-novpn
-    start-zen-proxy
-    start-zen-spotify
     start-zen-whats
   ];
 }
