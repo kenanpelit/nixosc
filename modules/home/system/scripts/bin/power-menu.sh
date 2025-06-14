c5f27fbc76c958929bd731825a0434ff38bf5580
kenanpelit <kenanpelit@gmail.com>
2025-06-14 14:26:59 +0300

    fix: improve rofi power menu readability with white text styling

--- modules/home/system/scripts/bin/power-menu.sh
+++ modules/home/system/scripts/bin/power-menu.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-rofi -show p -modi p:'rofi-power-menu' -theme-str 'window {width: 12em; height: 18em;} listview {lines: 5;}'
+rofi -show p -modi p:'rofi-power-menu' -theme-str 'window {width: 28em; height: 24em;}
+                listview {lines: 1;}
+                element-text {text-color: #ffffff;}
+                element {text-color: #ffffff;}
+                prompt {text-color: #ffffff;}
+                inputbar {text-color: #ffffff;}'
