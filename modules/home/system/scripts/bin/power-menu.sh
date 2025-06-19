0da49b64e23805e3b68a3fd274805c1aacb3dac1
kenanpelit <kenanpelit@gmail.com>
2025-06-20 00:06:41 +0300

    Fix rofi power menu display width and improve confirmation dialog

--- modules/home/system/scripts/bin/power-menu.sh
+++ modules/home/system/scripts/bin/power-menu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rofi -show p -modi p:'rofi-power-menu' -theme-str 'window {width: 28em; height: 24em;}
+rofi -show p -modi p:'rofi-power-menu' -theme-str 'window {width: 35em; height: 24em;}
                 listview {lines: 1;}
                 element-text {text-color: #ffffff;}
                 element {text-color: #ffffff;}
