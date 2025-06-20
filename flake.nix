d0277f53d7132ba76299d26bc0981efe5c9aed3d
kenanpelit <kenanpelit@gmail.com>
2025-06-20 17:59:59 +0300

    hyprland: 0620 - 6209 Commits

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
     # === Desktop Environment - Hyprland Ecosystem ===
     # Core Hyprland Wayland compositor - pinned to specific commit for stability
     hyprland = {
-      url = "github:hyprwm/hyprland/b49d0ca20e4378db1e9abeb661d72f4c8c070db9"; # 0620 - 6206 Commits
+      url = "github:hyprwm/hyprland/8ebff1948ff665ff6a1b49fb715b7de0797fae04"; # 0620 - 6209 Commits
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
