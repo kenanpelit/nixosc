dd059a14f52625af172db970093dc2339a4c7853
kenanpelit <kenanpelit@gmail.com>
2025-07-11 10:09:07 +0300

    power menu lavender

--- modules/home/scripts/bin/power-menu.sh
+++ modules/home/scripts/bin/power-menu.sh
@@ -4,5 +4,5 @@ rofi -show p -modi p:'rofi-power-menu' -theme-str 'window {width: 35em; height: 
                 listview {lines: 1;}
                 element-text {text-color: #ffffff;}
                 element {text-color: #ffffff;}
-                prompt {text-color: #ffffff;}
+                prompt {text-color: #b4befe; background-color: #1e1e2e; border-radius: 6px; padding: 8px;}
                 inputbar {text-color: #ffffff;}'
