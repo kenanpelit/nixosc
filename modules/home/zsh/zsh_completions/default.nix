e3fb941d13bd94e0768ffc72034182ac7ea8e0f0
kenanpelit <kenanpelit@gmail.com>
2025-07-28 22:58:58 +0300

    fix: update zsh dotDir to use absolute paths

--- modules/home/zsh/zsh_completions/default.nix
+++ modules/home/zsh/zsh_completions/default.nix
@@ -2,12 +2,12 @@
 {
   home.file = {
     #".config/zsh/completions/_iwctl".source = ./_iwctl;
-    ".config/zsh/completions/_assh".source = ./_assh;
+    "${config.xdg.configHome}/zsh/completions/_assh".source = ./_assh;
     # Diğer completion dosyaları için:
     # ".config/zsh/completions/other-completion.zsh".source = ./other-completion.zsh;
   };
   programs.zsh.initContent = ''
-    fpath+=~/.config/zsh/completions
+    fpath+=${config.xdg.configHome}/zsh/completions
     autoload -Uz compinit && compinit
   '';
 }
