1e15ceb70e2e7f2c2ee161da944b9bde74a068b2
kenanpelit <kenanpelit@gmail.com>
2025-08-03 00:49:05 +0300

    feat(spice): add KVM integration and enhanced security wrappers

--- modules/core/spice/default.nix
+++ modules/core/spice/default.nix
@@ -7,27 +7,46 @@
 # - Device agent
 # - Security wrappers
 # - Device rules
+# - KVM/QEMU integration
 #
 # Author: Kenan Pelit
 # ==============================================================================
-
 { pkgs, ... }:
 {
   # SPICE USB Redirection
   virtualisation.spiceUSBRedirection.enable = true;  # USB device passthrough
-
+  
   services = {
     # SPICE agent for guest integration
     spice-vdagentd.enable = true;
     
     # Device Rules
     udev.extraRules = ''
+      # USB devices for libvirtd group
       SUBSYSTEM=="usb", ATTR{idVendor}=="*", ATTR{idProduct}=="*", GROUP="libvirtd"
+      
+      # VFIO devices for GPU passthrough
       SUBSYSTEM=="vfio", GROUP="libvirtd"
+      
+      # Additional SPICE-specific rules
+      KERNEL=="kvm", GROUP="kvm", MODE="0664"
+      SUBSYSTEM=="misc", KERNEL=="vhost-net", GROUP="kvm", MODE="0664"
     '';
   };
-
+  
   # Security Configuration
-  security.wrappers.spice-client-glib-usb-acl-helper.source = 
-    "${pkgs.spice-gtk}/bin/spice-client-glib-usb-acl-helper";
+  security.wrappers.spice-client-glib-usb-acl-helper = {
+    source = "${pkgs.spice-gtk}/bin/spice-client-glib-usb-acl-helper";
+    capabilities = "cap_fowner+ep";
+    owner = "root";
+    group = "root";
+  };
+  
+  # SPICE packages
+  environment.systemPackages = with pkgs; [
+    spice-gtk          # SPICE client libraries
+    spice-protocol     # SPICE protocol headers
+    virt-viewer        # Remote desktop viewer
+  ];
 }
+
