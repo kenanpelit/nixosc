7d28ca3b9af716aef16c33177706cefcbe309ca8
kenanpelit <kenanpelit@gmail.com>
2025-10-16 13:50:40 +0300

    fix: semsumo launcher

--- modules/home/scripts/bin/semsumo.sh
+++ modules/home/scripts/bin/semsumo.sh
@@ -29,8 +29,8 @@ readonly SCRIPTS_DIR="$HOME/.nixosc/modules/home/scripts/start"
 readonly LOG_DIR="$HOME/.logs/semsumo"
 readonly LOG_FILE="$LOG_DIR/semsumo.log"
 readonly DEFAULT_FINAL_WORKSPACE="2"
-readonly DEFAULT_WAIT_TIME=2
-readonly DEFAULT_APP_TIMEOUT=7
+readonly DEFAULT_WAIT_TIME=1
+readonly DEFAULT_APP_TIMEOUT=5
 readonly DEFAULT_CHECK_INTERVAL=1
 
 # Colors
