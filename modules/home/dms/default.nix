bc55241708c4747ef6fb80c6792bc2183220f58a
kenanpelit <kenanpelit@gmail.com>
2025-12-05 02:08:16 +0300

    Autostart DankMaterialShell via user service

--- modules/home/dms/default.nix
+++ modules/home/dms/default.nix
@@ -5,6 +5,7 @@
 { inputs, lib, config, ... }:
 let
   cfg = config.my.user.dms;
+  dmsPkg = inputs.dankMaterialShell.packages.${config.pkgs.stdenv.hostPlatform.system}.default or inputs.dankMaterialShell.packages.${pkgs.stdenv.hostPlatform.system}.default;
 in
 {
   # Always import the upstream DMS Home Manager module; actual enable is gated below
@@ -16,5 +17,33 @@ in
 
   config = lib.mkIf cfg.enable {
     programs.dankMaterialShell.enable = true;
+
+    # Autostart DMS for the user session
+    home.packages = [ dmsPkg ];
+
+    systemd.user.services.dms = {
+      Unit = {
+        Description = "DankMaterialShell";
+        After = [ "graphical-session.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = "${dmsPkg}/bin/dms";
+        Restart = "on-failure";
+        RestartSec = 3;
+        Environment = [
+          "XDG_RUNTIME_DIR=/run/user/%U"
+          "XDG_CURRENT_DESKTOP=Hyprland"
+          "XDG_SESSION_TYPE=wayland"
+        ];
+        PassEnvironment = [
+          "WAYLAND_DISPLAY"
+          "HYPRLAND_INSTANCE_SIGNATURE"
+          "HYPRLAND_SOCKET"
+        ];
+      };
+      Install.WantedBy = [ "graphical-session.target" ];
+    };
   };
 }
