bb4b5c8bcd58c8294e09d9c98ee1e9853b58adc2
kenanpelit <kenanpelit@gmail.com>
2025-12-05 01:22:10 +0300

    Re-enable touchegg, remove lisgd

--- homes/x86_64-linux/kenan@hay/default.nix
+++ homes/x86_64-linux/kenan@hay/default.nix
@@ -41,11 +41,7 @@
   my.user.swayosd.enable = true;
   my.user.wpaperd.enable = true;
   my.user.waypaper.enable = true;
-  my.user.touchegg.enable = false;
-  my.user.lisgd = {
-    enable = true;
-    device = "/dev/input/event9";
-  };
+  my.user.touchegg.enable = true;
   my.user.dms.enable = true;
   my.user.fusuma.enable = true;
   my.user.blue.enable = true;
