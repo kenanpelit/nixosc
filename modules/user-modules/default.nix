9aac380400eafa7810e212ba0bdde91f63a9a917
kenanpelit <kenanpelit@gmail.com>
2025-12-04 13:22:13 +0300

    Make secrets path flake-relative and tighten module imports

--- modules/user-modules/default.nix
+++ modules/user-modules/default.nix
@@ -3,9 +3,12 @@
   imports = let
     path = ./.;
     dir = builtins.readDir path;
+    skipNames = [ "experimental" "archive" ".git" ".direnv" ];
     validFiles = lib.filterAttrs (name: type:
-      (type == "directory") ||
-      (type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix")
+      !(lib.hasPrefix "." name) &&
+      !(lib.elem name skipNames) &&
+      ((type == "directory") ||
+       (type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix"))
     ) dir;
   in
     lib.mapAttrsToList (name: type: path + "/${name}") validFiles;
