5db0a167568db28805b30182650b84321476332d
kenanpelit <kenanpelit@gmail.com>
2025-12-05 13:24:58 +0300

    search: varsayilan dsearch config ekle

--- modules/home/search/default.nix
+++ modules/home/search/default.nix
@@ -10,6 +10,51 @@
 { inputs, config, lib, pkgs, ... }:
 let
   cfg = config.my.user.search;
+  homeDir = config.home.homeDirectory;
+
+  # Varsayılan dsearch konfigürasyonu; kullanıcı cfg.dsearchConfig ile üzerine yazabilir
+  defaultDsearchConfig = {
+    index_path = "${homeDir}/.cache/danksearch/index";
+    listen_addr = ":43654";
+    max_file_bytes = 2097152; # 2MB
+    worker_count = 4;
+    index_all_files = true;
+    auto_reindex = false;
+    reindex_interval_hours = 24;
+
+    text_extensions = [
+      ".txt" ".md" ".go" ".py" ".js" ".ts"
+      ".jsx" ".tsx" ".json" ".yaml" ".yml"
+      ".toml" ".html" ".css" ".rs" ".c"
+      ".cpp" ".h" ".java" ".rb" ".php" ".sh"
+    ];
+
+    index_paths = [
+      {
+        path = homeDir;
+        max_depth = 6;
+        exclude_hidden = true;
+        exclude_dirs = [
+          "node_modules" "__pycache__" "venv" "target" "dist" "build" ".cache"
+          ".git" ".direnv" ".nix-defexpr"
+        ];
+      }
+      {
+        path = "${homeDir}/repos";
+        max_depth = 8;
+        exclude_hidden = true;
+        exclude_dirs = [
+          "node_modules" "venv" "target" ".git" "dist" "build" ".direnv" ".cache"
+        ];
+      }
+      {
+        path = "${homeDir}/Documents";
+        max_depth = 0; # sınırsız
+        exclude_hidden = false;
+        exclude_dirs = [ ];
+      }
+    ];
+  };
 in
 {
   # Bring in the upstream dsearch Home Manager module
@@ -35,7 +80,8 @@ in
 
     programs.dsearch = {
       enable = true;
-      config = cfg.dsearchConfig;
+      # Kullanıcı ayarlarıyla birleştirilmiş varsayılan TOML
+      config = lib.recursiveUpdate defaultDsearchConfig cfg.dsearchConfig;
     };
   };
 }
