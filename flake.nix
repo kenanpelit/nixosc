9776e45a85d3e225a3cb46458d2a1f7b68c1e298
kenanpelit <kenanpelit@gmail.com>
2025-12-06 04:11:01 +0300

    Revert "Drop nvtop from Ax-Shell package"
    This reverts commit 762b90e1015fd955d43d2737d7057b4ba12b0c4f.

--- flake.nix
+++ flake.nix
@@ -211,17 +211,10 @@
         # Overlays applied to all systems
         overlays = with inputs; [
           nur.overlays.default
-          (final: prev:
-            let
-              axShellPkg = inputs.ax-shell.packages.${prev.stdenv.hostPlatform.system}.default;
-              droppedNvtop = prev.lib.remove prev.nvtop (axShellPkg.buildInputs or [ ]);
-            in {
-              # Provide ax-shell package for modules expecting pkgs.ax-shell
-              ax-shell = axShellPkg.overrideAttrs (old: {
-                # Ax-Shell pulls in nvtop (NVIDIA-centric). Drop it for non-NVIDIA hosts.
-                buildInputs = droppedNvtop;
-              });
-            })
+          (final: prev: {
+            # Provide ax-shell package for modules expecting pkgs.ax-shell
+            ax-shell = inputs.ax-shell.packages.${prev.stdenv.hostPlatform.system}.default;
+          })
         ];
 
         # Modules automatically added to all NixOS systems
