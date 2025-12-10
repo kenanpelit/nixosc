82e4657aea94c3dc4472f94eba4598c1457c4327
kenanpelit <kenanpelit@gmail.com>
2025-12-10 20:25:53 +0300

    cleanup

--- modules/home/scripts/bin/askpass.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# askpass.sh - rofi tabanlı sudo parola istemi
-# GUI gerektirmeden dmenu modunda parola alır; sudo için askpass olarak
-# kullanılabilir.
-rofi -dmenu -password -p "Sudo Password:" -theme-str 'entry { placeholder: "Enter sudo password..."; }' -theme-str 'window { width: 30%; }'
