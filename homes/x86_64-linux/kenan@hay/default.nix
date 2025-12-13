335f2605017e832c7cd2982d78a05461c9350dac
kenanpelit <kenanpelit@gmail.com>
2025-12-13 16:23:53 +0300

    Remove sunsetr option reference

--- homes/x86_64-linux/kenan@hay/default.nix
+++ homes/x86_64-linux/kenan@hay/default.nix
@@ -84,7 +84,6 @@
   my.user.nemo.enable = true;
   my.user.yazi.enable = true;
   my.user.rsync.enable = true;
-  my.user.sunsetr.enable = false;
 
   # Productivity
   my.user.obsidian.enable = true;
