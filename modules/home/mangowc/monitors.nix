5001cbe17c286a4a4fd104d8f88620b43599b8b1
kenanpelit <kenanpelit@gmail.com>
2025-12-26 19:22:18 +0300

    mangowc: enable on hay + set default monitor rules

--- modules/home/mangowc/monitors.nix
+++ modules/home/mangowc/monitors.nix
@@ -7,18 +7,20 @@
 #
 # You can list output names in a Mango session with:
 #   mmsg -O
+#
+# Ref: https://github.com/DreamMaoMao/mango/wiki/
 # ==============================================================================
 { lib, ... }:
 
 {
   hardwareDefault = ''
     # ==============================================================================
-    # Monitor rules (disabled by default)
+    # Monitor rules
     # ==============================================================================
-    #
-    # Example:
-    # monitorrule=DP-3,0.55,1,tile,0,1.0,0,0,2560,1440,165
-    # monitorrule=eDP-1,0.55,1,tile,0,1.0,2560,0,1920,1080,60
+    # NOTE: Adjust output names/sizes/refresh to your actual setup.
+    #       List outputs in a Mango session: `mmsg -O`
+
+    monitorrule=DP-3,0.55,1,tile,0,1.0,0,0,2560,1440,165
+    monitorrule=eDP-1,0.55,1,tile,0,1.0,2560,0,1920,1080,60
   '';
 }
-
