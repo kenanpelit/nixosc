39df3284421ce7e84527881a68f0c58b2655448e
kenanpelit <kenanpelit@gmail.com>
2026-01-11 19:56:51 +0300

    hypr: map Mod+M to maximize column

--- modules/home/hyprland/binds.nix
+++ modules/home/hyprland/binds.nix
@@ -39,7 +39,7 @@ let
     # Actions
     "$mainMod, Q, killactive"
     "$mainMod, F, fullscreen, 0"
-    "$mainMod, M, fullscreen, 1"       # Maximize (Fake fullscreen)
+    "$mainMod, M, layoutmsg, colresize 1.0" # Maximize Column (Niri Mod+M match)
     "$mainMod, C, layoutmsg, togglefit" # Center/Fit Column (Niri Mod+C match)
   ];
 
