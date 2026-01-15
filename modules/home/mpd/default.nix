fa3a808dfe6185d1148ad47fd0cfed39a63c6d78
kenanpelit <kenanpelit@gmail.com>
2026-01-16 02:44:03 +0300

    update various

--- modules/home/mpd/default.nix
+++ modules/home/mpd/default.nix
@@ -32,6 +32,13 @@ in
           name "PipeWire Sound Server"
           mixer_type "software"
         }
+
+        audio_output {
+          type "fifo"
+          name "Visualizer"
+          path "/tmp/mpd.fifo"
+          format "44100:16:2"
+        }
         
         # Playback Settings
         restore_paused "yes"
