a3235d02c6b7e7165f9ead67fa7706e93dce3500
kenanpelit <kenanpelit@gmail.com>
2026-01-16 03:16:55 +0300

    update various

--- modules/home/mpd/default.nix
+++ modules/home/mpd/default.nix
@@ -21,6 +21,7 @@ in
     services.mpd = {
       enable = true;
       musicDirectory = "${config.home.homeDirectory}/Music";
+      playlistDirectory = "${config.home.homeDirectory}/Music/playlists";
       
       # ---------------------------------------------------------------------------
       # Audio and Performance Settings
