8da9b40b11d0734e9165df4d6921ff2daea1d43e
kenanpelit <kenanpelit@gmail.com>
2026-01-17 15:30:38 +0300

    hyprland: keep border on single/maximized

--- modules/home/hyprland/settings.nix
+++ modules/home/hyprland/settings.nix
@@ -34,9 +34,10 @@ let
     (map (n: mkWorkspaceEntry { monitor = primaryMonitorDesc; index = n; isDefault = n == 1; }) (lib.range 1 6))
     ++ (map (n: mkWorkspaceEntry { monitor = secondaryMonitorDesc; index = n; isDefault = n == 7; }) (lib.range 7 9))
     ++ [
-      # Smart Gaps & Borders - No gaps/borders/shadow/rounding when only one window is present
-      "w[tv1], gapsout:0, gapsin:0, bordersize:0, rounding:0, shadow:0"
-      "f[1], gapsout:0, gapsin:0, bordersize:0, rounding:0, shadow:0"
+      # Smart gaps (single / maximized):
+      # Keep borders so 0.8-width single windows still have a visible focus outline.
+      "w[tv1], gapsout:0, gapsin:0, rounding:0, shadow:0"
+      "f[1], gapsout:0, gapsin:0, rounding:0, shadow:0"
 
       # Smart borders - Disabled to show borders always
       # "w[v1]s[false], bordersize:0, rounding:false"
