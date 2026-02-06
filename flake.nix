4cf612b767ce7a962a2aa7e599dafb5649d09150
kenanpelit <kenanpelit@gmail.com>
2026-02-06 23:22:25 +0300

    dankMaterialShell: update to latest 0206

--- flake.nix
+++ flake.nix
@@ -125,12 +125,12 @@
     dankMaterialShell = {
       inputs.nixpkgs.follows = "nixpkgs";
       # Pinned commit (updated via `osc-fiup dank`)
-      url = "github:AvengeMedia/DankMaterialShell/c5a21f8da0eaf26e7d9c84553e5aee79b7b1c2e4"; # 0206 - Updated commit
+      url = "github:AvengeMedia/DankMaterialShell/e618a8390c0eccec1661a0bbd4d2b81cb77cbe86"; # 0206 - Updated commit
+      # url = "github:AvengeMedia/DankMaterialShell/c5a21f8da0eaf26e7d9c84553e5aee79b7b1c2e4"; # 0206 - Updated commit
       # url = "github:AvengeMedia/DankMaterialShell/ac84cadd77d3b1daf4a1da1a54294c991d7a5b32"; # 0206 - Updated commit
       # url = "github:AvengeMedia/DankMaterialShell/e27e90415714c52ed90a94eaf8b6db57f4423a71"; # 0205 - Updated commit
       # url = "github:AvengeMedia/DankMaterialShell/fe156679866fd5633a2c2a70c765410c22ee356a"; # 0204 - Updated commit
       # url = "github:AvengeMedia/DankMaterialShell/6e3b3ce8886f35b9ecfa1c8ac0b781cc7f9b74e8"; # 0204 - Updated commit
-      # url = "github:AvengeMedia/DankMaterialShell/c024c1b8e478697c0bb56c2d068dc6725a3c2f92"; # 0204 - Updated commit
     };
 
     dsearch = {
